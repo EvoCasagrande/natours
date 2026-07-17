9e5af62cd7bd20af69803c8dce8ac639af545883
Evo <casagrandeevo@gmail.com>
2026-07-17 13:01:11 -0300

    Se instalo Slugify y se agregaron Middlewares en el Schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,56 +1,84 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true,
-        trim: true,
+        name: {
+            type: String,
+            required: [true, 'A tour must have a name'],
+            unique: true,
+            trim: true,
+        },
+        duration: {
+            type: Number,
+            required: [true, 'A tour must have a duration'],
+        },
+        maxGroupSize: {
+            type: Number,
+            required: [true, 'A tour must have a group size'],
+        },
+        difficulty: {
+            type: String,
+            required: [true, 'A tour must have a difficulty'],
+        },
+        ratingAverage: {
+            type: Number,
+            default: 4.5,
+        },
+        ratingQuantity: {
+            type: Number,
+            default: 0,
+        },
+        price: {
+            type: Number,
+            required: [true, 'A tour must have a price'],
+        },
+        priceDiscount: Number,
+        summary: {
+            type: String,
+            trim: true,
+        },
+        description: {
+            type: String,
+            trim: true,
+        },
+        imageCover: {
+            type: String,
+            required: [true, 'A tour must have a cover image'],
+        },
+        images: [String],
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        startDates: [Date],
+        secretTour: {
+            type: Boolean,
+            default: false,
+        },
     },
-    duration: {
-        type: Number,
-        required: [true, 'A tour must have a duration'],
-    },
-    maxGroupSize: {
-        type: Number,
-        required: [true, 'A tour must have a group size'],
-    },
-    difficulty: {
-        type: String,
-        required: [true, 'A tour must have a difficulty'],
-    },
-    ratingAverage: {
-        type: Number,
-        default: 4.5,
-    },
-    ratingQuantity: {
-        type: Number,
-        default: 0,
-    },
-    price: {
-        type: Number,
-        required: [true, 'A string must have a price'],
-    },
-    priceDiscount: Number,
-    summary: {
-        type: String,
-        trim: true,
-    },
-    description: {
-        type: String,
-        trim: true,
-    },
-    imageCover: {
-        type: String,
-        required: [true, 'A tour must have a cover image'],
-    },
-    images: [String],
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-    },
-    startDates: [Date],
+    {
+       toJSON: { virtuals: true },
+       toObject: { virtuals: true },
+    }
+);
+
+//VIRTUAL PROPERTIES
+tourSchema.virtual('durationWeeks').get(function () {
+    return this.duration / 7;
 });
+
+//DOCUMENT MIDDLEWARE
+tourSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+})
+
+//QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function (doc, next) {
+    this.find({ secretTour: { $ne: true } });
+    next();
+})
+
 
 const Tour = mongoose.model('Tour', tourSchema);
 
