7388637ef1ad25a7634e101264740ed4113ce0a1
Evo <casagrandeevo@gmail.com>
2026-07-29 13:13:44 -0300

    Se agrego login y endpoints en users

--- models/userModel.js
+++ models/userModel.js
@@ -1,44 +1,43 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: [true, 'Please tell us your name! ']
-        },
-        email: {
-            type: String,
-            required: [true, 'Please provide your email'],
-            unique: [true, 'The email provided is already in use'],
-            lowercase: true,
-            validate: [validator.isEmail, 'Please provide a valid email']
-        },
-        photo: {
-            type: String,
-        },
-        password: {
-            type: String,
-            required : [true, 'Please provide a password'],
-            minlength: 8,
-        },
-        passwordConfirm: {
-            type: String,
-            required: [true, 'Please confirm your password'],
-            validate: {
-                validator: function (el) {
-                    return el === this.password;
-                }
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tell us your name! '],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide your email'],
+        unique: [true, 'The email provided is already in use'],
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email'],
+    },
+    photo: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8,
+        select: false,
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            validator: function (el) {
+                return el === this.password;
             },
-            message: 'Passwords are not the same'
-        }
-    }
-);
+        },
+        message: 'Passwords are not the same',
+    },
+});
 
 userSchema.pre('save', async function (next) {
     // Only run this function if password was modified
-    if(!this.isModified('password')) return next();
+    if (!this.isModified('password')) return next();
 
     // Hash the password with cost of 12
     this.password = await bcrypt.hash(this.password, 12);
@@ -47,6 +46,13 @@ userSchema.pre('save', async function (next) {
     this.passwordConfirm = undefined;
     next();
 });
+
+userSchema.methods.correctPassword = async function (
+    candidatePassword,
+    userPassword,
+) {
+    return bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
