cc56fe3529c234ca04ab78c15e5ea0d578e549f1
Evo <casagrandeevo@gmail.com>
2026-08-06 11:35:59 -0300

    Se agrego seguridad

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const globalErrorController = require('./controllers/errorController');
 const AppError = require('./utils/appError');
@@ -10,10 +14,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //1) GLOBAL MIDDLEWARES
-app.use(express.json());
+// Set security
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+// Limit request from same API
 
 const limiter = rateLimit({
     max: 100,
@@ -23,6 +32,30 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+    hpp({
+        whitelist: [
+            'duration',
+            'ratingQuantity',
+            'ratingAverage',
+            'maxGroupSize',
+            'difficulty',
+            'price',
+        ],
+    }),
+);
+
+// Serving static files
 app.use(express.static('public'));
 
 //2) RUTAS
